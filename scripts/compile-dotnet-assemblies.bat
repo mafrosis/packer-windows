e57b3982349a9a96f603071ee7751bf418ddc464
mafro <github@mafro.net>
2014-10-28 10:25:03 +1100

    compile as admin

--- scripts/compile-dotnet-assemblies.bat
+++ scripts/compile-dotnet-assemblies.bat
@@ -2,12 +2,12 @@
 ::http://robrelyea.wordpress.com/2007/07/13/may-be-helpful-ngen-exe-executequeueditems/
 
 if exist %windir%\microsoft.net\framework\v4.0.30319\ngen.exe (
-	%windir%\microsoft.net\framework\v4.0.30319\ngen.exe update /force /queue
-	%windir%\microsoft.net\framework\v4.0.30319\ngen.exe executequeueditems
+	runas /env /user:vagrant /savecred %windir%\microsoft.net\framework\v4.0.30319\ngen.exe update /force /queue
+	runas /env /user:vagrant /savecred %windir%\microsoft.net\framework\v4.0.30319\ngen.exe executequeueditems
 )
 if exist %windir%\microsoft.net\framework64\v4.0.30319\ngen.exe (
-	%windir%\microsoft.net\framework64\v4.0.30319\ngen.exe update /force /queue
-	%windir%\microsoft.net\framework64\v4.0.30319\ngen.exe executequeueditems
+	runas /env /user:vagrant /savecred %windir%\microsoft.net\framework64\v4.0.30319\ngen.exe update /force /queue
+	runas /env /user:vagrant /savecred %windir%\microsoft.net\framework64\v4.0.30319\ngen.exe executequeueditems
 )
 
 :: continue even if ngen fails
